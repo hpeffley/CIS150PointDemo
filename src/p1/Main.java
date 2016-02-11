6061ac0af5f27528d3fb187dd7316435ea62d493
hpeffley <peffhenry@verizon.net>
2016-02-11 10:33:57 -0500

    Initial commit

---
+++ src/p1/Main.java
@@ -0,0 +1,18 @@
+package p1;
+
+import javafx.geometry.Point2D;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Point2D pt1 = new Point2D(3, 0);
+        Point2D pt2 = new Point2D(0, 4);
+        double hypotenuse = pt1.distance(pt2);
+
+        System.out.println(pt1.toString());
+
+        System.out.println(pt1);
+        System.out.printf("hypotenuse = %.2f%n",
+                hypotenuse);
+    }
+}
